bf516f8456afe0662315c45617dc49a0934f0575
MaxwellAlan <alanmaxwell@163.com>
2018-03-12 22:53:26 +0800

    fix blog index

--- myblog/models.py
+++ myblog/models.py
@@ -3,30 +3,49 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
+# 分类
 class Category(models.Model):
     name=models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
+#标签
 class Tag(models.Model):
     name=models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
+#博客
 class Post(models.Model):
+    #文章标题
     title=models.CharField(max_length=100,verbose_name="标题")
+    #文章内容
     body=models.TextField(verbose_name="博客内容")
+    #发表时间
     created_time=models.DateTimeField(default=timezone.now,verbose_name="发表时间")
+    #修改时间
     modified_time=models.DateTimeField(verbose_name="最后修改时间")
+    #摘要
     excerpt=models.CharField(max_length=200,blank=True,verbose_name="摘要")
+    #分类
     category=models.ForeignKey(Category,on_delete=models.CASCADE,verbose_name="分类")
+    #标签
     tages=models.ManyToManyField(Tag,blank=True,verbose_name="标签")
+    #作者
     author=models.ForeignKey(User,related_name="blog_posts",on_delete=models.CASCADE,verbose_name="作者")
+    # 阅读量
+    view_times = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering=("-created_time",)
+
+    def increase_view_times(self):
+        self.views += 1
+        self.save(update_fields=['view_times'])
 
     def __str__(self):
         return self.title
--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Category, Post, Tag
 # Create your views here.
 def blog(request):
-    return render(request, 'blog/index.html')
+    category_list = Category.objects.all()
+    tag_list = Tag.objects.all()
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html',
+                  context={'post_list': post_list, 'tag_list': tag_list, 'category_list': category_list})
 
 
 def signin(request):
